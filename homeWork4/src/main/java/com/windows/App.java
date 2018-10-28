c5738c7983b0b823ceaa1001b35d2f87a4db3064
Alex <alex_gsp@ukr.net>
2018-10-29 00:14:29 +0200

    Mario Version 3.0

--- homeWork4/src/main/java/com/windows/App.java
+++ homeWork4/src/main/java/com/windows/App.java
@@ -12,7 +12,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        Hero.setVersion("2.0");
+        Hero.setVersion("3.0");
         Hero hero = new Hero();
     }
 }
