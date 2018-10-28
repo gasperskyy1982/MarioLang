49210616b36b4ee37f379d50280f65b680ea3bd2
Alex <alex_gsp@ukr.net>
2018-10-29 00:11:26 +0200

    Mario Version 1.0

---
+++ homeWork4/src/main/java/com/windows/App.java
@@ -0,0 +1,18 @@
+package com.windows;
+
+/**
+ * Hello world!
+ *
+ */
+
+import homeWork_1.*;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Hero.setVersion("1.0");
+        Hero hero = new Hero();
+    }
+}
